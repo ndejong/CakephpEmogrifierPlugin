e98f23ef47692893ceebf0fe92873088ac23db2f
Nicholas de Jong <nicholas.dejong@gmail.com>
2011-12-19 14:28:40 +1000

    initial commit

---
+++ README.markdown
@@ -0,0 +1,50 @@
+CakephpEmogrifierPlugin
+=======================
+
+CakephpEmogrifierPlugin is a CakePHP 2.0x Plugin that makes of using Emogrify on
+your HTML output easy.
+
+Wondering what Emogrify is?  Emogrifier is a great library from Pelago that deals
+with much of the hassle involved with HTML formatted email messages:-
+ - http://www.pelagodesign.com/sidecar/emogrifier/
+
+This Plugin is a wrapper around Emogrifier making it easy to use in CakePHP 2.0x
+
+
+Install
+=======
+
+### Step 1
+Copy or symlink CakephpEmogrifierPlugin into a path named Emogrifier in your Plugin
+path like this:-
+>  app/Plugin/Emogrifier
+
+Take careful note of the Plugin pathname, the name is "Emogrifier", not 
+EmogrifierPlugin or CakephpEmogrifierPlugin, it's just Emogrifier.  I spell this 
+out because it's an easy thing to trip up on especially if your pulling this down 
+from github or unpacking from a tarball.
+
+### Step 2
+Be sure to load the plugin in your bootstrap.php or core.php, like this:-
+>  CakePlugin::load('Emogrifier');
+
+### Step 3
+Tell your controller to render your view with Emogrifier like this:-
+>  $this->viewClass = 'Emogrifier.Emogrifier';
+
+
+Questions and Answers
+=====================
+Q: Pelago's Emogrifier class requires you to pass in the CSS but this plugin does
+   not, what's going on?
+A: We parse the HTML from the View->output attribute looking for CSS from <link>
+   and <style> elements then stich the whole thing together - it makes using this
+   a little easier.
+
+Q: I'm using Emogrifier before rendering to webpages and I can't see any difference
+A: Take a look at the HTML source, you should notice that all your CSS styles are
+   now inline element style attributes.
+
+Q: Why does this Plugin have no Tests?
+A: Because call me daft, I just can't work out how to write a PHPUnit test for 
+   output from a Plugin View - let me know if you do!
---
+++ View/EmogrifierView.php
@@ -0,0 +1,253 @@
+<?php
+
+/* ***************************************************************************
+ * Cakephp EmogrifierPlugin
+ * Nicholas de Jong - http://nicholasdejong.com - https://github.com/ndejong
+ * 19 December 2011
+ * 
+ * @author Nicholas de Jong
+ * @copyright Nicholas de Jong
+ * ***************************************************************************/
+
+App::uses('View', 'View');
+
+/**
+ * ### Usage
+ * 
+ * class ExampleController extends AppController {
+ * 
+ * 		public function index () {
+ *			$this->viewClass = 'Emogrifier.Emogrifier';
+ * 		}
+ * }
+ * 
+ */
+class EmogrifierView extends View {
+	
+	/**
+	 * $media_types - if a <link> element has a media attribute, we only 
+	 * slurp up the CSS those that match these
+	 * 
+	 * @var array
+	 */
+	public $media_types = array('all','screen');
+	
+	/**
+	 * $remove_css - remove the original css from HTML, includes the removal 
+	 * of <link> and <style> elements
+	 * 
+	 * @var bool
+	 */
+	public $remove_css = true;
+	
+	/**
+	 * $import_external_css - determines if we attempt to pull down external @import css
+	 * 
+	 * @var bool
+	 */
+	public $import_external_css = false;
+
+	/**
+	 * Constructor
+	 *
+	 * @param Controller $controller The controller with viewVars
+	 */
+	public function __construct($controller = null) {
+		parent::__construct($controller);
+	}
+
+	/**
+	 * Render with Emogrification
+	 *
+	 * @param string $view
+	 * @param string $layout
+	 * @return mixed
+	 * @throws NotFoundException
+	 */
+	public function render($view = null, $layout = null) {
+
+		// Let the parent do it's rendering thing first
+		parent::render($view, $layout);
+		
+		// Parse out the CSS into a string and remove any CSS from the output
+		$css = $this->_extractAndRemoveCss();
+
+		// Import the Emogrifier class
+		App::import('Emogrifier.Vendor', 'Emogrifier', array('file' => 'emogrifier.php'));
+		$Emogrifier = new Emogrifier($this->output, $css);
+
+		// Emogrification!
+		$this->output = @$Emogrifier->emogrify();
+
+		//debug($this->output);
+		return $this->output;
+	}
+	
+	/**
+	 * _extractAndRemoveCss - extracts any CSS from the rendered view and 
+	 * removes it from the $this->output
+	 * 
+	 * @return string
+	 */
+	protected function _extractAndRemoveCss() {
+		
+		$html = $this->output;
+		$css = null;
+		
+		$DOM = new DOMDocument;
+		$DOM->loadHTML($html);
+		
+		// DOM removal queue
+		$remove_doms = array();
+		
+		// catch <link> style sheet content
+		$links = $DOM->getElementsByTagName('link');
+		
+		foreach($links as $link) {
+			if($link->hasAttribute('href') && preg_match("/\.css$/i",$link->getAttribute('href'))) {
+				
+				// find the css file and load contents
+				if($link->hasAttribute('media')) {
+					foreach($this->media_types as $css_link_media) {
+						if(strstr($link->getAttribute('media'),$css_link_media)) {
+							$css .= $this->_findAndLoadCssFile($link->getAttribute('href'))."\n\n";
+							$remove_doms[] = $link;
+						}
+					}
+				}
+				else {
+					$css .= $this->_findAndLoadCssFile($link->getAttribute('href'))."\n\n";
+					$remove_doms[] = $link;
+				}
+			}
+		}
+		
+		// Catch embeded <style> and @import CSS content
+		$styles = $DOM->getElementsByTagName('style');
+		
+		// Style
+		foreach($styles as $style) {
+			if($style->hasAttribute('media')) {
+				foreach($this->media_types as $css_link_media) {
+					if(strstr($style->getAttribute('media'),$css_link_media)) {
+						$css .= $this->_parseInlineCssAndLoadImports($style->nodeValue);
+						$remove_doms[] = $style;
+					}
+				}
+			}
+			else {
+				$css .= $this->_parseInlineCssAndLoadImports($style->nodeValue);
+				$remove_doms[] = $style;
+			}
+		}
+		
+		// Remove
+		if($this->remove_css) {
+			foreach($remove_doms as $remove_dom) {
+				try {
+					$remove_dom->parentNode->removeChild($remove_dom);
+				} catch (DOMException $e) {}
+			}
+			// Throw the new output back onto $this->output
+			$this->output = $DOM->saveHTML();
+		}
+		
+		//debug($css);exit;
+		return $css;
+	}
+	
+	/**
+	 * _findAndLoadCssFile - finds the appropriate css file within the CSS path
+	 * 
+	 * @param string $css_href 
+	 */
+	protected function _findAndLoadCssFile($css_href) {
+
+		$css_filenames = array_merge($this->_globRecursive(CSS.'*.Css'),$this->_globRecursive(CSS.'*.CSS'),$this->_globRecursive(CSS.'*.css'));
+		
+		// Build an array of the ever more path specific $css_href location
+		$css_hrefs = split(DS,$css_href);
+		$css_href_paths = array();
+		for($i=count($css_hrefs)-1;$i>0;$i--) {
+			if(isset($css_href_paths[count($css_href_paths)-1])) {
+				$css_href_paths[] = $css_hrefs[$i].DS.$css_href_paths[count($css_href_paths)-1];
+			}
+			else {
+				$css_href_paths[] = $css_hrefs[$i];
+			}
+		}
+		
+		// the longest string match will be the match we are looking for
+		$best_css_filename = null;
+		$best_css_match_length = 0;
+		foreach($css_filenames as $css_filename) {
+			foreach($css_href_paths as $css_href_path) {
+				$regex = "/".str_replace('/','\/',str_replace('.','\.',$css_href_path))."/";
+				if(preg_match($regex,$css_filename,$match)) {
+					if(strlen($match[0]) > $best_css_match_length) {
+						$best_css_match_length = strlen($match[0]);
+						$best_css_filename = $css_filename;
+					}
+				}
+			}
+		}
+		
+		$css = null;
+		if(!empty($best_css_filename) && is_file($best_css_filename)) {
+			$css = file_get_contents($best_css_filename);
+		}
+		
+		return $css;
+	}
+	
+	/**
+	 * _globRecursive
+	 * 
+	 * @param string $pattern
+	 * @param int $flags
+	 * @return array 
+	 */
+	protected function _globRecursive($pattern, $flags = 0) {
+
+		$files = glob($pattern, $flags);
+
+		foreach (glob(dirname($pattern) . '/*', GLOB_ONLYDIR | GLOB_NOSORT) as $dir) {
+			$files = array_merge($files, $this->_globRecursive($dir . '/' . basename($pattern), $flags));
+		}
+
+		return $files;
+	}
+	
+	/**
+	 * _parseInlineCssAndLoadImports
+	 */
+	protected function _parseInlineCssAndLoadImports($css) {
+		
+		// Remove any <!-- --> comment tags - they are valid in HTML but we probably 
+		// don't want to be commenting out CSS
+		$css = str_replace('-->','',str_replace('<!--','',$css))."\n\n";
+		
+		// Load up the @import CSS if any exists
+		preg_match_all("/\@import.*?url\((.*?)\)/i",$css,$matches);
+		
+		if(isset($matches[1]) && is_array($matches[1])) {
+			
+			// First remove the @imports
+			$css = preg_replace("/\@import.*?url\(.*?\).*?;/i",'', $css);
+			
+			foreach($matches[1] as $url) {
+				if(preg_match("/^http/i",$url)) {
+					if($this->import_external_css) {
+						$css .= file_get_contents($url);
+					}
+				}
+				else {
+					$css .= $this->_findAndLoadCssFile($url);
+				}
+			}
+		}
+		
+		return $css;
+	}
+
+}
